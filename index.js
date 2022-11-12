bc025723961c04e2eb72459fb42a989c228dc756
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-11-12 18:28:24 -0300

    tarefa finalizada

---
+++ index.js
@@ -0,0 +1,13 @@
+const numeroDigitado = prompt("FALE UM NUMERO :");
+let n2 = 0;
+let contador =1;
+let resultado = 'Tabuada do número escolhido:';
+ 
+while(n2 < 10)
+{
+    n2 = n2 + 1;
+    contador = numeroDigitado * n2;
+    resultado = `${resultado}\n${numeroDigitado} x ${n2} = ${contador}`;
+}
+
+alert(resultado);
